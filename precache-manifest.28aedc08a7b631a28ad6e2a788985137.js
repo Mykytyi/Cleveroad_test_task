f7eeabc1ec7e100eb0e3069d6bd9b21cc6ed2047
Nikita <cherevaty.nik@gmail.com>
2019-08-21 18:30:17 +0300

    Updates

---
+++ precache-manifest.28aedc08a7b631a28ad6e2a788985137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f61e247dab465a961f1abdbfc26730e",
+    "url": "/Cleveroad_test_task/index.html"
+  },
+  {
+    "revision": "11344af34571a41eff4d",
+    "url": "/Cleveroad_test_task/static/css/main.95ac2473.chunk.css"
+  },
+  {
+    "revision": "a47e458f1e764a464812",
+    "url": "/Cleveroad_test_task/static/js/2.b15025c0.chunk.js"
+  },
+  {
+    "revision": "11344af34571a41eff4d",
+    "url": "/Cleveroad_test_task/static/js/main.1264f56f.chunk.js"
+  },
+  {
+    "revision": "c89feffd377a3e3c0051",
+    "url": "/Cleveroad_test_task/static/js/runtime~main.ab1d0ad5.js"
+  },
+  {
+    "revision": "50ae75fde984eab67d7fefc72d533691",
+    "url": "/Cleveroad_test_task/static/media/astr.50ae75fd.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Cleveroad_test_task/precache-manifest.28aedc08a7b631a28ad6e2a788985137.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cleveroad_test_task/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
